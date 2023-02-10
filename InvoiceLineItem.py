f9c07427f2389c418cb0d23d402f6a000a91eef8
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-09 20:19:13 -0800

    final code

--- InvoiceLineItem.py
+++ InvoiceLineItem.py
@@ -1,2 +1,35 @@
 
-Janise Adams CIS261 InvoiceLineItem
+def getprice():
+    while True:
+        try:
+            price = float(input("Enter Price: "))
+            return price
+        except ValueError:
+            print("Invalid Decimal Number. Please Try Again.")
+
+def getquantity():
+    while True:
+        try:
+            quantity = int(input("Enter Quantity: "))
+            return quantity
+        except ValueError:
+            print("Invalid Integar. Please Try Again.")
+def main():
+    print("The Invoice Line Item Calculator")
+    print()
+    answer = "y"
+    while answer.lower() == "y":
+        price = getprice()
+        quantity = getquantity()
+        totals = price*quantity
+
+        print()
+        print("PRICE: ", f"{price: .2f}")
+        print("QUANTITY: ", quantity)
+        print("TOTAL: ", f"{totals: .2f}")
+        answer = input("Enter Another Line Item? (y/n): ")
+        print()
+    print("Bye!!")
+
+if __name__ == "__main__":
+    main()
